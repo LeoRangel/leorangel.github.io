34c3d7c7494ed796ac97933f966aaf13877a1f92
LeoRangel <leandro.mdrs06@gmail.com>
2022-01-02 20:37:51 -0300

    Create "back to top" button and fix scripts

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,32 @@
+//Get elements
+const backToTopBtn = document.getElementById("back-to-top-btn");
+const sidebar = document.getElementById("sidebar");
+
+// When the user clicks on the button, scroll to the top of the document
+const backToTopFunction = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// When the user scrolls down 200px from the top of the document, show the button
+const scrollFunction = () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.classList.add('show-btn');
+    } else {
+        backToTopBtn.classList.remove('show-btn');
+    }
+}
+
 // Open sidebar function
 const sidebar_open = () => {
     document.documentElement.style.overflowY = "hidden";
-    document.getElementById("sidebar").style.right = "0px";
+    sidebar.classList.add("open-sidebar");
 }
 
 // Close sidebar function
 const sidebar_close = () => {
     document.documentElement.style.overflowY = "visible";
-    document.getElementById("sidebar").style.right = "-100vw";
+    sidebar.classList.remove("open-sidebar");
 }
 
 window.onload = () => {
@@ -23,8 +42,12 @@ window.onload = () => {
     document.querySelectorAll('#sidebar ul li a').forEach(link => {
         link.addEventListener('click', () => sidebar_close())
     });
+
+    // Set sidebar links events
+    backToTopBtn.addEventListener('click', () => backToTopFunction());
 };
 
+window.onscroll = () => scrollFunction();
 
 
 
